f3aae84dd8312c0a3e3f79c6e6247365d44316ff
shunt <shunt@twilio.com>
2020-08-01 01:07:47 -0700

    a

--- osmolarity/src/Calculator.jsx
+++ osmolarity/src/Calculator.jsx
@@ -1,150 +1,241 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Item } from './Item';
+import Item from './Item';
+import styled, { css } from 'styled-components'
+
+export const MG_PER_ML_NAME = "mg per ml"
+export const MSOM_PER_ML_NAME = "msoms / ml"
+export const NUM_CCs_NAME = "#cc's"
+export const MSOM_NAME = "msom"
+export const PRICE_PER_ML_NAME = "price per ml"
+export const TOTAL_COST_NAME = "Total Cost"
+
+const header_data = {
+    name: "NAME",
+    amount: "AMOUNT",
+    mg_per_ml: MG_PER_ML_NAME,
+    msom_per_ml : MSOM_PER_ML_NAME,
+    num_ccs : NUM_CCs_NAME,
+    msom : MSOM_NAME,
+    price_per_ml : PRICE_PER_ML_NAME,
+    total_cost : TOTAL_COST_NAME
+}
+
+const Container = styled.div`
+    text-align: center;
+`
+
+const Table = styled.div`
+    /* text-align: center; */
+    display: flex;
+    flex-direction: "column";
+    /* align-items: stretch; */
+    /* align-content: stretch;
+    justify-content: center; */
+`
 
 function Calculator() {
-    const items = {
-        "Ascorbic Acid" : {
+    //     name: "Ascorbic Acid",
+    //     amount: 0,
+    //     MG_PER_ML: 500,
+    //     MSOM_PER_ML : 6.05,
+    //     NUM_CCs : 0,
+    //     MSOM : 0,
+    //     PRICE_PER_ML : .44,
+    //     TOTAL_COST : 0
+    let chemicals = [
+        {
+            name: "Ascorbic Acid",
             amount: 0,
-            "mg per ml": 500,
-            "msoms / ml" : 6.05,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : .44,
-            "Total Cost" : 0
+            mg_per_ml: 500,
+            msom_per_ml : 6.05,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : .44,
+            total_cost : 0
         },
-        "Magnesium Chloride" : {
+        {
+            name: "Magnesium Chloride",
             amount: 0,
-            "mg per ml": 200,
-            "msoms / ml" : 2.95,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 200,
+            msom_per_ml : 2.95,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "Calcium Chloride" : {
+        {
+            name: "Calcium Chloride",
             amount: 0,
-            "mg per ml": 100,
-            "msoms / ml" : 2.04,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 100,
+            msom_per_ml : 2.04,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "B-complex" : {
+        {
+            name: "B-complex",
             amount: 0,
-            "mg per ml": 100,
-            "msoms / ml" : 2.04,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 100,
+            msom_per_ml : 2.04,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "B6" : {
+        {
+            name: "B6",
             amount: 0,
-            "mg per ml": 100,
-            "msoms / ml" : 1.6,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 100,
+            msom_per_ml : 1.6,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "B5" : {
+        {
+            name: "B5",
             amount: 0,
-            "mg per ml": 250,
-            "msoms / ml" : 1.36,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 250,
+            msom_per_ml : 1.36,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "B12" : {
+        {
+            name: "B12",
             amount: 0,
-            "mg per ml": 5,
-            "msoms / ml" : 2,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 5,
+            msom_per_ml : 2,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "Taurine" : {
+        {
+            name: "Taurine",
             amount: 0,
-            "mg per ml": 50,
-            "msoms / ml" : 0.95,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 50,
+            msom_per_ml : 0.95,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "Zinc Chloride" : {
+        {
+            name: "Zinc Chloride",
             amount: 0,
-            "mg per ml": 10,
-            "msoms / ml" : 0.21,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 10,
+            msom_per_ml : 0.21,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "Selenium" : {
+        {
+            name: "Selenium",
             amount: 0,
-            "mg per ml": 0.2,
-            "msoms / ml" : 0.1,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 0.2,
+            msom_per_ml : 0.1,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "Alpha Lipoic Acid" : {
+        {
+            name: "Alpha Lipoic Acid",
             amount: 0,
-            "mg per ml": 25,
-            "msoms / ml" : 0.12,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 25,
+            msom_per_ml : 0.12,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "L-arginine" : {
+        {
+            name: "L-arginine",
             amount: 0,
-            "mg per ml": 200,
-            "msoms / ml" : 3.02,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 200,
+            msom_per_ml : 3.02,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "L-carnitine" : {
+        {
+            name: "L-carnitine",
             amount: 0,
-            "mg per ml": 500,
-            "msoms / ml" : 10.05,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: 500,
+            msom_per_ml : 10.05,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "Amino blend" : {
+        {
+            name: "Amino blend",
             amount: 0,
-            "mg per ml": "Glutamine 30 m/Ornithine 50 mg/Arginine 100 mg/lysine 50 mg/Citrulline 50 mg/Levocarnitine 100 mg",
-            "msoms / ml" : 4.4,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 1.63,
-            "Total Cost" : 0
+            mg_per_ml: "Glutamine 30 m/Ornithine 50 mg/Arginine 100 mg/lysine 50 mg/Citrulline 50 mg/Levocarnitine 100 mg",
+            msom_per_ml : 4.4,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 1.63,
+            total_cost : 0
         },
-        "Biotin" : {
+        {
+            name: "Biotin",
             amount: 0,
-            "mg per ml": 0.5,
-            "msoms / ml" : 0.011,
-            "#cc's" : 0,
-            "msom" : 0,
-            "price per ml" : 2.50,
-            "Total Cost" : 0
+            mg_per_ml: 0.5,
+            msom_per_ml : 0.011,
+            num_ccs : 0,
+            msom : 0,
+            price_per_ml : 2.50,
+            total_cost : 0
+        }
+    ]
+
+    function update_and_calculate(new_chemical) {
+        let additives = 0
+        let total_msoms = 0
+        let total_cost = 0.0
+        let osmolarity = 0
+
+        for (let i = 0; i < chemicals.length(); i++) {
+
+            if (chemicals[i].name === new_chemical.name){
+                chemicals[i] = new_chemical
+            }
+
+            const chem = chemicals[i]
+
+            chem.msom = chem.msom_per_ml * chem.num_ccs
+            chem.total_cost = chem.price_per_ml * chem.num_ccs
+
+
+            additives += chem.num_ccs
+            total_msoms += chem.msom
+            total_cost += chem.total_cost
+        }
+
+        osmolarity = (total_msoms / additives) * 1000
+
+        return {
+            // chemicals,
+            total_msoms,
+            total_cost,
+            osmolarity
         }
     }
 
+
     return (
-        <div className="Calculator">
-            <Item></Item>
-        </div>
+        <Container className="Calculator">
+            <Table>
+                <Item header data={header_data} />
+                {chemicals.map(item => <Item data={item} update={update_and_calculate}/>)}
+            </Table>
+        </Container>
     );
 }
 
--- osmolarity/src/Item.jsx
+++ osmolarity/src/Item.jsx
@@ -1,11 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled, { css } from 'styled-components'
 
-function Item() {
+const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0.5em 1em;
+    padding: 0.25em 1em;
+    ${props => props.primary && css`
+        background: palevioletred;
+        color: white;
+    `}
+`;
+const Container = styled.div`
+    text-align: center;
+`
+
+const Row = styled.div`
+    /* display: flex; */
+    /* flex-direction: row; */
+    /* flex-grow: 1; */
+`
+
+const Header = styled.th`
+    /* display: flex; */
+    /* flex-direction: row; */
+    /* flex-grow: 1; */
+`
+
+const Input = styled.input``
+
+
+// render(
+//   <Container>
+//     <Button>Normal Button</Button>
+//     <Button primary>Primary Button</Button>
+//   </Container>
+// );
+
+function Item({data, header}) {
+    //     name: "Ascorbic Acid",
+    //     amount: 0,
+    //     MG_PER_ML: 500,
+    //     MSOM_PER_ML : 6.05,
+    //     NUM_CCs : 0,
+    //     MSOM : 0,
+    //     PRICE_PER_ML : .44,
+    //     TOTAL_COST : 0
+
+    if (header){
+        return (
+            <Header>
+                <span>{data.name}</span>
+                <span> {data.amount} </span>
+                {/* <span>{data.mg_per_ml}</span> */}
+                <span>{data.msoms_per_ml}</span>
+                <span>{data.num_ccs}</span>
+                <span>{data.msom}</span>
+                <span>{data.price_per_ml}</span>
+                <span>{data.total_cost}</span>
+            </Header>
+        )
+    }
+
     return (
-        <div className="Item">
-
-        </div>
+        <Row>
+            <span>{data.name}</span>
+            <Input value={data.amount}/>
+            {/* <span>{data.mg_per_ml}</span> */}
+            <span>{data.msoms_per_ml}</span>
+            <span>{data.num_ccs}</span>
+            <span>{data.msom}</span>
+            <span>{data.price_per_ml}</span>
+            <span>{data.total_cost}</span>
+        </Row>
     );
 }
 
